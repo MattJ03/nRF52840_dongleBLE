873dc64a4b3868e1439940d435d32f1daa30a9e2
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-16 20:27:44 +0000

    initial commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dongle_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/home/matt/Downloads/ncs/zephyr/samples/basic/blinky/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/home/matt/zephyr-python/bin/python3.12" CACHE INTERNAL "NCS child image controlled")
---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,23 @@
+empty_file.o: /home/matt/Downloads/ncs/zephyr/misc/empty_file.c \
+ /home/matt/Downloads/ncs/zephyr/boards/arm/nrf52840dongle_nrf52840/nrf52840dongle_nrf52840.dts \
+ /home/matt/Downloads/ncs/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/matt/Downloads/ncs/zephyr/dts/common/mem.h \
+ /home/matt/Downloads/ncs/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/matt/Downloads/ncs/zephyr/dts/arm/armv7-m.dtsi \
+ /home/matt/Downloads/ncs/zephyr/dts/common/skeleton.dtsi \
+ /home/matt/Downloads/ncs/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/sys/util_macro.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/sys/util_internal.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/sys/util_loops.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/matt/Downloads/ncs/zephyr/dts/common/freq.h \
+ /home/matt/Downloads/ncs/zephyr/dts/arm/nordic/override.dtsi \
+ /home/matt/Downloads/ncs/zephyr/boards/arm/nrf52840dongle_nrf52840/nrf52840dongle_nrf52840-pinctrl.dtsi \
+ /home/matt/Downloads/ncs/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/matt/Downloads/ncs/zephyr/boards/arm/nrf52840dongle_nrf52840/fstab-stock.dtsi
