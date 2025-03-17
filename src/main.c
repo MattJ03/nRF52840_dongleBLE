b251f5e4021faa2ecc1307150084571d75820c99
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-17 22:58:49 +0000

    check if BLE connection failed

--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
 #include <zephyr/types.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <sys/printk.h>
+#include <bluetooth/blutetooth.h>
+#include <bluetooth/hci.h>
+
+void main(void) {
+	int err;
+
+	printf("Starting Bluetooth scan...\n");
+
+	//enabling bluetooth
+
+	err = BT_enable(NULL);
+	if(err) {
+		printf("Bluetooth initialisation failed (err %d)\n", err);
+		return;
+	}
+
+	
+}
