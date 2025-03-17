7c9354464e90162be8c704d613e83ec7ee5f2b2f
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-17 23:01:19 +0000

    completed ble code

--- src/main.c
+++ src/main.c
@@ -18,5 +18,21 @@ void main(void) {
 		return;
 	}
 
-	
+	printf("Bluetooth initialised\n");
+
+	struct bt_le_adv_param *adv_params = BT_LE_ADV_NCONN_IDENTITY;
+    struct bt_data adv_data[] = {
+        BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+        BT_DATA(BT_DATA_NAME_COMPLETE, "Zephyr_Beacon", 14),
+    };
+
+    // Start advertising
+    err = bt_le_adv_start(adv_params, adv_data, ARRAY_SIZE(adv_data), NULL, 0);
+    if (err) {
+        printk("Advertising failed (err %d)\n", err);
+        return;
+    }
+
+    printk("Beacon is now advertising!\n");
+}
 }
